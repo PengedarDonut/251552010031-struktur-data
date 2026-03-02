2bcb0cc8cd562b2f56df4789fbea74ea499aa11f
Muhammad Dimas <dimasputras2006@gmail.com>
2026-03-02 07:14:19 +0700

    Delete copy dictionary.py

--- copy dictionary.py
+++
@@ -1,7 +0,0 @@
-def copy_dictionary():
-    data = {"Pengedar" : "08123456789", "Donat" : "0912345678"}
-    return data
-
-data_user = copy_dictionary()
-copy_data = data_user.copy()
-print("Setelah di copy : ", copy_data)
