72f08769ea9f1e76bc67edf0615546283ffb2928
Muhammad Dimas <dimasputras2006@gmail.com>
2026-03-04 19:23:03 +0700

    Delete item.py

--- item.py
+++
@@ -1,6 +0,0 @@
-def items_dictionary():
-    User = {"Pengedar" : "0812345678", "Donut" : "09123456789"}
-    return User
-
-kontak = items_dictionary()
-print("Items kontak : ", list(kontak.items()))
