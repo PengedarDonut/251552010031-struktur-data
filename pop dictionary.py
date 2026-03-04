27768d5c3fcd42c8b3df155a6c0ea6fc7fa30b4b
Muhammad Dimas <dimasputras2006@gmail.com>
2026-03-04 19:24:24 +0700

    Delete pop dictionary.py

--- pop dictionary.py
+++
@@ -1,6 +0,0 @@
-def pop_dictionary():
-    User = {"Pengedar" : "0812345678", "Donut" : "09123456789"}
-    return User
-data_user = pop_dictionary()
-hapus = data_user.pop("Pengedar")
-print("Setelah di hapus : ", data_user)
