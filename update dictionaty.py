c1f7682195a0697437f589a8ec849ea68ca5f0e8
Muhammad Dimas <dimasputras2006@gmail.com>
2026-03-04 19:24:50 +0700

    Delete update dictionaty.py

--- update dictionaty.py
+++
@@ -1,7 +0,0 @@
-def update_dictionary():
-    User = {"Pengedar" : "0812345678", "Donut" : "09123456789"}
-    return User
-
-data_user = update_dictionary()
-data_user.update({"Enak" : "0712345678"})
-print("Setelah di update : ", data_user)
