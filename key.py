b7c50a3371de320a3c8dd814deb4c70c3c17df64
Muhammad Dimas <dimasputras2006@gmail.com>
2026-03-04 19:24:08 +0700

    Delete key.py

--- key.py
+++
@@ -1,6 +0,0 @@
-def key_dictionary():
-    User = {"Pengedar" : "0812345678", "Donut" : "09123456789"}
-    return User
-
-data_user = key_dictionary()
-print("Keys kontak : ", list(data_user.keys()))
