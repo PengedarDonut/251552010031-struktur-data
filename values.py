70331f151d1c167633ca6eb7d354d1d7ac11f1d7
Muhammad Dimas <dimasputras2006@gmail.com>
2026-03-04 19:25:01 +0700

    Delete values.py

--- values.py
+++
@@ -1,6 +0,0 @@
-def values_dictionary():
-    User = {"Pengedar" : "0812345678", "Donut" : "09123456789"}
-    return User
-
-data_user = values_dictionary()
-print("Values user : ", list(data_user.values()))
